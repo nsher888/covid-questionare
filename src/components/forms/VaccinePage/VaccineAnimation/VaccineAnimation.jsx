141db126c9ec16ee1bf7f699ed3c0410258397be
Nikoloz Shervashidze <51024662+nsher888@users.noreply.github.com>
2023-06-05 21:49:55 +0400

    feat: add star animation to the page

--- src/components/forms/VaccinePage/VaccineAnimation/VaccineAnimation.jsx
+++ src/components/forms/VaccinePage/VaccineAnimation/VaccineAnimation.jsx
@@ -1,5 +1,5 @@
-import backgroundImage from '@/assets/scan2.png';
-import rectangle from '@/assets/identification-rectangle.png';
+import backgroundImage from '@/assets/doctor2.png';
+import rectangle from '@/assets/star.png';
 import { motion } from 'framer-motion';
 
 const IdentificationAnimation = () => {
@@ -7,13 +7,13 @@ const IdentificationAnimation = () => {
     <div className='relative flex-1'>
       <motion.img
         src={rectangle}
-        initial={{ opacity: 0, width: 0, top: 200, right: 700 }}
-        animate={{ opacity: 0.8, width: 622, top: 80, right: 350 }}
+        initial={{ opacity: 0, width: 0, top: 210, right: 600 }}
+        animate={{ opacity: 0.8, width: 288, top: -25, right: 250 }}
         transition={{ duration: 0.5 }}
-        className='absolute left-0 opacity-80 top-20'
+        className='absolute left-6 opacity-80 top-11'
         alt='Rectangle'
       />
-      <img src={backgroundImage} className='-mt-20' alt='Ilustration' />
+      <img src={backgroundImage} className='mt-3' alt='Ilustration' />
     </div>
   );
 };
